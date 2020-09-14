13e31e94c3e5956abbdf0fc1fe5e1dba3fce2bf7
fazttech <fazt@faztweb.com>
2020-09-14 18:29:38 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const Stripe = require("stripe");
+const stripe = new Stripe("<your_secretkey_here>");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+
+app.post("/api/checkout", async (req, res) => {
+  // you can get more data to find in a database, and so on
+  const { id, amount } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Gaming Keyboard",
+      payment_method: id,
+      confirm: true, //confirm the payment at the same time
+    });
+
+    console.log(payment);
+
+    return res.status(200).json({ message: "Successful Payment" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: error.raw.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server on port", 3001);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootswatch/dist/lux/bootstrap.min.css";
+import "./App.css";
+
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+import axios from "axios";
+
+const stripePromise = loadStripe("<your public key here>");
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+    setLoading(true);
+
+    if (!error) {
+      // console.log(paymentMethod)
+      const { id } = paymentMethod;
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3001/api/checkout",
+          {
+            id,
+            amount: 10000, //cents
+          }
+        );
+        console.log(data);
+
+        elements.getElement(CardElement).clear();
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+  };
+
+  console.log(!stripe || loading);
+
+  return (
+    <form className="card card-body" onSubmit={handleSubmit}>
+      {/* Product Information */}
+      <img
+        src="https://www.corsair.com/medias/sys_master/images/images/h80/hdd/9029904465950/-CH-9109011-ES-Gallery-K70-RGB-MK2-01.png"
+        alt="Corsair Gaming Keyboard RGB"
+        className="img-fluid"
+      />
+
+      <h3 className="text-center my-2">Price: 100$</h3>
+
+      {/* User Card Input */}
+      <div className="form-group">
+        <CardElement />
+      </div>
+
+      <button disabled={!stripe} className="btn btn-success">
+        {loading ? (
+          <div className="spinner-border text-light" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          "Buy"
+        )}
+      </button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className="container p-4">
+        <div className="row h-100">
+          <div className="col-md-4 offset-md-4 h-100">
+            <CheckoutForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
   );
 }
 
